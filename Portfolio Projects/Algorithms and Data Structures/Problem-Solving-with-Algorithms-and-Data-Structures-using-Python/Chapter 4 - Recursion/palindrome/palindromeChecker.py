655118f62853fca113525fd61e9674cf92d1cf77
Random-Baka <79850313+Random-Baka@users.noreply.github.com>
2022-07-27 18:33:20 +0100

    Refactoring Palindrome

--- Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/palindrome/palindromeChecker.py
+++ Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/palindrome/palindromeChecker.py
@@ -1,6 +1,7 @@
 paldindromeList = ["redivider", "deified", "civic", "radar", "level", "rotor", "kayak", "reviver", "racecar", "madam", "refer"]
+from random import randrange
+# I will write a couple different approaches to solve the paldindrom problem.
 
-# I will write a couple different approaches to solve
 # Approach One - deque
 from pythonds.basic import Deque
 
@@ -21,10 +22,7 @@ def paldindromeWithDeck(wordToCheck=paldindromeList[0]):
     stringToRet = "Word: " + wordToCheck + (" is a paldindrome"  if isPalindrome else " is not a paldindrome")
     return stringToRet
 
-def palindromeWithSlicing():
-    # Base case
-    print("1")
-
+# Approach Two Itteration
 def palindromeWithItteration(wordToCheck=paldindromeList[0]):
     """This function uses the itterative approach to solve the problem
     again this doesn't use recursion, but is a simple to code method"""
@@ -42,3 +40,26 @@ def palindromeWithItteration(wordToCheck=paldindromeList[0]):
 
     stringToRet = "Word: " + wordToCheck + (" is a paldindrome"  if isPalindrome else " is not a paldindrome")
     return stringToRet
+
+# Approach Three Slicing
+def palindromeWithSlicing(wordToCheck):
+    """Uses recursion to solve the palindrome problem. 
+    It does this by slicing the remaining chars and feeding it back into itself.
+    Eventualy ethier the word will be 1 or 0 len and thus return true or index 0 and -1 
+    won't match and it will return false."""
+    # Base case
+    if len(wordToCheck) <= 1:
+        return True
+    else:
+        if wordToCheck[0] == wordToCheck[-1]:
+            return palindromeWithSlicing(wordToCheck[1:-1])
+        else:
+            return False
+
+# Gen random number to select word from list.
+randomNum = randrange(0, len(paldindromeList)-1)
+
+# Calculate and print results
+print(paldindromeWithDeck(paldindromeList[randomNum]) + "\ntested with deque\n")
+print(palindromeWithItteration(paldindromeList[randomNum]) + "\ntested with itteration\n")
+print("Word: {} is {} paldindrome".format(paldindromeList[randomNum], ("a" if palindromeWithSlicing(paldindromeList[randomNum]) else "not a")) + "\ntested with slicing\n")
