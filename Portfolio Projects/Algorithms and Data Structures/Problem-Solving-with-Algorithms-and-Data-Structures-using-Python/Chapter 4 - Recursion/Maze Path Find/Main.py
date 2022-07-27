cf5cae19e910d235cf6144c480328ba23d4203af
Random-Baka <79850313+Random-Baka@users.noreply.github.com>
2022-07-27 16:43:22 +0100

    Update to readme

--- Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/Maze Path Find/Main.py
+++ Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/Maze Path Find/Main.py
@@ -1,9 +1,16 @@
 from Maze import *
 from MazeSearch import *
 
-newMaze = Maze('mazeLayoutOne.txt')
-newMaze.drawMaze()
-newMaze.updatePosition(newMaze.startRow, newMaze.startCol)
+def main():
+    # Create new maze from file and show on the screen
+    newMaze = Maze('mazeLayoutOne.txt')
+    newMaze.drawMaze()
+    newMaze.updatePosition(newMaze.startRow, newMaze.startCol)
+    
+    # Start recursive call
+    searchFrom(newMaze, newMaze.startRow, newMaze.startCol)
+    # Keeps turtle window open after it completes
+    newMaze.screenExit.exitonclick()
 
-searchFrom(newMaze, newMaze.startRow, newMaze.startCol)
-newMaze.screenExit.exitonclick()
+if __name__ == "__main__":
+    main()
--- Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/Maze Path Find/README.md
+++ Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/Maze Path Find/README.md
@@ -1,4 +1,6 @@
 <center>
     <h3>Path finding through a maze</h3>
     <h3>using recursive approach</h3>
+    <br>
+    <p>This is not a super efficient method of pathfinding. It was more a demonstration of recursion, though it does, in time, find a path <br>out of the maze</p>
 </center>
