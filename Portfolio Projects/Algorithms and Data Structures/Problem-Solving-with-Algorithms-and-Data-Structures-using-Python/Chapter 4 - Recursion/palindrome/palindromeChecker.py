071fb02e9e9abe46fae9dfdaa93761248c49cd56
Random-Baka <79850313+Random-Baka@users.noreply.github.com>
2022-07-27 18:09:17 +0100

    xcfabvg

--- Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/palindrome/palindromeChecker.py
+++ Portfolio Projects/Algorithms and Data Structures/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/Chapter 4 - Recursion/palindrome/palindromeChecker.py
@@ -2,7 +2,43 @@ paldindromeList = ["redivider", "deified", "civic", "radar", "level", "rotor", "
 
 # I will write a couple different approaches to solve
 # Approach One - deque
-from pythonds.basic import deque
+from pythonds.basic import Deque
 
-deckOne = deque(paldindromeList[0])
-print(deckOne.removeFront())
+def paldindromeWithDeck(wordToCheck=paldindromeList[0]):
+    """This function uses the deque(deck) data structure to pop
+    both sides of the word until ethier the word has 1 or less chars or
+    the chars are not equal. Though it doesn't use recursion, I like
+    this solution to the palindrome problem."""
+    isPalindrome = True
+    deckOne = Deque()
+    for ch in paldindromeList[0]:
+        deckOne.addRear(ch)
+
+    while (deckOne.size() > 1 and isPalindrome):
+        charsAreEqual = deckOne.removeFront() == deckOne.removeRear()
+        if not charsAreEqual:
+            isPalindrome = False
+    stringToRet = "Word: " + wordToCheck + (" is a paldindrome"  if isPalindrome else " is not a paldindrome")
+    return stringToRet
+
+def palindromeWithSlicing():
+    # Base case
+    print("1")
+
+def palindromeWithItteration(wordToCheck=paldindromeList[0]):
+    """This function uses the itterative approach to solve the problem
+    again this doesn't use recursion, but is a simple to code method"""
+    # Initial state settings
+    isPalindrome = True
+    firstChar = 0
+    lastChar = len(wordToCheck) - 1
+
+    while firstChar < lastChar and isPalindrome:
+        if wordToCheck[firstChar] != wordToCheck[lastChar]:
+            isPalindrome = False
+            continue
+        firstChar += 1
+        lastChar -= 1
+
+    stringToRet = "Word: " + wordToCheck + (" is a paldindrome"  if isPalindrome else " is not a paldindrome")
+    return stringToRet
